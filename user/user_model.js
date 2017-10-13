9810ea169c26c14296e099f1a9d27fcf8a85c1b5
Joey Jirasevijinda <joey.jira@gmail.com>
2017-10-13 15:27:08 -0700

    fixed user model

--- user/user_model.js
+++ user/user_model.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
         required: true,
         unique: true,
         lowercase: true
-    }
+    },
     password: {
         type: String,
         required: true
